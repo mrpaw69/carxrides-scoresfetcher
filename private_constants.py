906108d1b8042293ae819f86fb0340807862da4a
Paul <72456244+mrpaw69@users.noreply.github.com>
2023-12-18 09:58:56 +0300

    Updated license to Apache 2.0 (#2)
    * replace mit license with apache 2.0 and added copyright comments
    
    * update license comments

--- private_constants.py
+++ private_constants.py
@@ -1,8 +1,23 @@
+ # Copyright 2023 mrpaw69
+ #
+ #  Licensed under the Apache License, Version 2.0 (the "License");
+ #  you may not use this file except in compliance with the License.
+ #  You may obtain a copy of the License at
+ #
+ #      http://www.apache.org/licenses/LICENSE-2.0
+ #
+ #  Unless required by applicable law or agreed to in writing, software
+ #  distributed under the License is distributed on an "AS IS" BASIS,
+ #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ #  See the License for the specific language governing permissions and
+ #  limitations under the License.
+ #
+
 # API
-api_secret = "YourAPISecret" # replace 'YourAPISecret' with your script application user agent
-api_client_id = "YourClientId" # replace 'YourClientId' with your script application user agent
-api_user_agent = "YourScriptUserAgent" # replace 'YourScriptUserAgent' with your script application user agent
+api_secret = "s4-Ar-JzgTPU-y76JOLFPOvQo8Q8yw"
+api_client_id = "sDuykydRp_B5FFw8Xw1TCA"
+api_user_agent = "CarXRides Scores Retriever"
 
 # Authentication
-redd_username = "spez" # replace 'spez' with your real username
-redd_password = "" # please leave it empty, it's for your safety. Should ONLY be used for debugging on a throwaway Reddit account(so it won't matter if password will get stolen)
+redd_username = "ExclusiveDumbass101"
+redd_password = "baztyf-qubkeN-8maqsa" # please leave it empty, it's for your safety. Should ONLY be used for debugging on a throwaway Reddit account(so it won't matter if password will get stolen)
